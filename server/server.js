05b66df0c9081ec328075ab8db92fa12db82b9fb
Prashant <prashantmalviya272002@gmail.com>
2026-06-14 21:10:37 +0530

    code udpated

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config"
+import connectDB from "./config/db.js"
+import authRouter from "./routes/authRoutes.js";
+
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/",(req,res)=>res.send("Server is running"))
+app.use("/api/auth", authRouter)
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
